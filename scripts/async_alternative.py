bc7f7869af02ab96ef88122b272b7abe5e5c0bea
J S <49557684+svilupp@users.noreply.github.com>
2024-10-05 18:47:51 +0100

    update examples

--- scripts/async_alternative.py
+++ scripts/async_alternative.py
@@ -1,6 +1,9 @@
 # Asynchronous Example
 import asyncio
 from mistralai import Mistral
+from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
+from tenacity import retry, stop_after_attempt, wait_exponential
+import json
 import os
 from pydantic import BaseModel, Field
 from typing import List
@@ -8,22 +11,36 @@ from typing import List
 class Painter(BaseModel):
     name: str = Field(description="The name of the French painter")
 
-async def get_best_french_painter():
-    s = Mistral(
-        api_key=os.getenv("MISTRAL_API_KEY", ""),
-    )
-    res = await s.chat.complete_async(model="mistral-small-latest", messages=[
+ # @retry(stop=stop_after_attempt(5), wait=wait_exponential(multiplier=2, min=1, max=10))
+@retry(
+    stop=stop_after_attempt(3),
+    wait=wait_fixed(1),
+    retry=retry_if_exception_type((ValueError, json.JSONDecodeError))
+)
+async def get_painter_with_retry(client):
+    tools = get_tools_and_choice(Painter)
+    res = await client.chat.complete_async(model="mistral-small-latest", messages=[
+        {
+            "content": f"""You are a helpful assistant that extracts information about painters from a given text.""",
+            "role": "system",
+        },
         {
             "content": "Who is the best French painter? Answer in one short sentence.",
             "role": "user",
         },
-    ], 
-    response_format="json_object", **get_tools_and_choice(Painter))
+    ], **tools)
     
-    return res.choices[0].message.content if res else None
-
-async def get_tools_and_choice(ResponseModel):
-    return {
+    content = res.choices[0].message.tool_calls[0].function.arguments
+    print(content)
+    try:
+        # Validate the response using Pydantic
+        painter = Painter.model_validate_json(content)
+        return painter
+    except Exception as e:
+        raise  # This will be caught by the retry decorator
+        
+def get_tools_and_choice(ResponseModel):
+    dictionary = {
         "tools": [
             {
                 "type": "function",
@@ -37,11 +54,15 @@ async def get_tools_and_choice(ResponseModel):
         "tool_choice": {
             "type": "function",
             "function": {"name": "ExtractPainter"},
-        },
+        }
     }
+    return dictionary
 
 async def main():
-    tasks = [get_best_french_painter() for _ in range(50)]
+    client = Mistral(
+        api_key=os.getenv("MISTRAL_API_KEY", ""),
+    )
+    tasks = [get_painter_with_retry(client) for _ in range(10)]
     results = await asyncio.gather(*tasks)
     
     for i, result in enumerate(results, 1):
@@ -49,3 +70,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+    
