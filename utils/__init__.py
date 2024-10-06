1b9ad9b69e380b7f75e6ffd39e1770b7d430733d
J S <49557684+svilupp@users.noreply.github.com>
2024-10-06 12:16:35 +0100

    update pptx utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,5 @@
 from .models import IssueCategory,IssueLocation, ExtractedIssue, DetectedIssue, ExtractedIssueList
 from .mocks import generate_mock_detected_issues
 from .image_utils import encode_image, get_image_data_url
+from .pptx_utils import extract_text_from_pptx
 from .client import MistralClientWrapper
